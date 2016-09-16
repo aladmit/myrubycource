59d5d4d57edbffc7ea14cd4c5c1599d12148e801
Andrey Aleksandrov <postgred@gmail.com>
2016-09-16 23:52:19 +0300

    Task 3

--- movies.rb
+++ movies.rb
@@ -1,4 +1,11 @@
 #!/home/andrey/.rbenv/shims/ruby
+require 'time'
+
+def print_films(films)
+  films.map do |film|
+    puts "#{film[:title]}: #{film[:producer]} (#{film[:date]}; #{film[:genre].join("/").to_s}) - #{film[:duration]} min"
+  end
+end
 
 file = ARGV[0] || "movies.txt"
 
@@ -7,12 +14,28 @@ unless File.exist?(file)
   exit
 end
 
-FIELDS = %i(url title year country date genre duration stars produces actors)
+films = []
+
+FIELDS = %i(url title year country date genre duration stars producer actors)
 
 File.open(file,'r').map do |line|
   film = FIELDS.zip(line.split('|')).to_h
-
-  if film[:title].include?("Time")
-    puts "title: #{film[:title]} starts: #{"*" * film[:stars].to_i}"
-  end
+  film[:duration] = film[:duration].split(' ')[0].to_i
+  film[:genre] = film[:genre].split(',')
+  films.push film
 end
+
+puts "Long films:"
+films.sort_by! { |film| film[:duration] }
+print_films(films.reverse[0..4])
+
+puts "Camedy"
+films.sort_by! { |film| film[:date] }
+print_films(films[0..9])
+
+puts "Producers"
+producers = []
+films.map { |film| producers.push film[:producer] }
+producers.sort_by { |man| man.split(' ')[-1] }.uniq.map { |man| puts man }
+
+puts films.count { |film| film[:country] != 'USA' }
