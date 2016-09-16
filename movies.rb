0bc943e811eb2edbf205bfffc39ce9a45ca53120
Andrey Aleksandrov <postgred@gmail.com>
2016-09-16 22:35:34 +0300

    replate each_line to map and rewrite hash

--- movies.rb
+++ movies.rb
@@ -1,34 +1,18 @@
 #!/home/andrey/.rbenv/shims/ruby
 
-file = ARGV[0].nil? ? "movies.txt" : ARGV[0]
+file = ARGV[0] || "movies.txt"
 
 unless File.exist?(file)
   puts "File #{file} does not exist"
   exit
 end
 
-films = []
+FIELDS = %w(url title year country date genre duration stars produces actors)
 
-File.open(file,'r').each_line do |line|
-  string = line.split('|')
-  film = {
-    url: string[0],
-    title: string[1],
-    year: string[2],
-    country: string[3],
-    date: string[4],
-    genre: string[5],
-    duration: string[6],
-    stars: string[7],
-    producer: string[8],
-    actors: string[9]
-  }
-  films.push film
-end
+File.open(file,'r').map do |line|
+  film = FIELDS.zip(line.split('|')).to_h
 
-films.each do |film|
-  if film[:title].include?("Time")
-    message = "title: #{film[:title]} starts: #{"*" * film[:stars].to_i}"
-    puts message
+  if film['title'].include?("Time")
+    puts "title: #{film['title']} starts: #{"*" * film['stars'].to_i}"
   end
 end
