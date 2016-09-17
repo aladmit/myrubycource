0a8812fa51898708d4a434925321b3ebdb2bdb9f
Andrey Aleksandrov <postgred@gmail.com>
2016-09-17 13:48:54 +0300

    replace hash to OpenStruct

--- movies.rb
+++ movies.rb
@@ -1,7 +1,10 @@
 #!/home/andrey/.rbenv/shims/ruby
+require 'csv'
+require 'ostruct'
+
 def print_films(films)
   films.map do |film|
-    puts "#{film[:title]}: #{film[:producer]} (#{film[:date]}; #{film[:genre].join("/")}) - #{film[:duration]} min"
+    puts "#{film.title}: #{film.producer} (#{film.date}; #{film.genre.join('/')}) - #{film.duration} min"
   end
 end
 
@@ -14,21 +17,19 @@ end
 
 FIELDS = %i(url title year country date genre duration stars producer actors)
 
-films = File.open(file, 'r').map do |line|
-  film = FIELDS.zip(line.split('|')).to_h
-  film[:duration] = film[:duration].split(' ')[0].to_i
-  film[:genre] = film[:genre].split(',')
+films = CSV.read(file, {col_sep: '|'}).map do |line|
+  film = OpenStruct.new(FIELDS.zip(line).to_h)
+  film.duration = film.duration.split(' ')[0].to_i
+  film.genre = film.genre.split(',')
   film
 end
 
 puts "Long films:"
-print_films(films.sort_by { |film| film[:duration] }.last(5))
+print_films(films.sort_by { |film| film.duration }.last(5))
 
 puts "Comedy:"
-print_films(films.select { |film| film[:genre].include?("Comedy") }.sort_by { |film| film[:date] }.first(10))
+print_films(films.select { |film| film.genre.include?("Comedy") }.sort_by { |film| film.date }.first(10))
 
 puts "Producers:"
-producers = films.map { |film| film[:producer] }.sort_by { |man| man.split(' ').last }.uniq
+producers = films.map { |film| film.producer }.sort_by { |man| man.split(' ').last }.uniq
 producers.map { |man| puts man }
-
-puts films.count { |film| film[:country] != 'USA' }
