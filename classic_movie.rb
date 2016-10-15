ca5bf6fd135eea090e61597685cacf8040cd36b2
Andrey Aleksandrov <postgred@gmail.com>
2016-10-15 15:02:41 +0300

    Add ClassicMovies#to_s

--- classic_movie.rb
+++ classic_movie.rb
@@ -1,4 +1,18 @@
 require './movie.rb'
 
 class ClassicMovie < Movie
+  def to_s
+    str = "#{title} - классический фильм, режиссер #{producer}\n"
+    str += "Other films of producer:\n"
+    producer_movies.each do |movie|
+      str += "  #{movie.title}\n"
+    end
+    str
+  end
+
+  def producer_movies
+    movies = @collection.filter(producer: producer)
+    movies.delete(self)
+    movies
+  end
 end
--- spec/classic_movie_spec.rb
+++ spec/classic_movie_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper.rb'
 require_relative '../movies.rb'
 
 RSpec.describe ClassicMovie do
-  before { @films = MovieCollection.new('./spec/movies.rb') }
-  subject(:movie) { @films.filter(:year, 1946..1968)[0] }
+  before { @films = MovieCollection.new('./spec/movies.txt') }
+  subject(:movie) { @films.filter(year: 1946..1968)[0] }
 
-  its(:to_s) { should eq "Angry Men - классический фильм, режиссер Sidney Lumet\nOther films of producer:\n  Network\n  Dog Day Afternoon" }
+  its(:to_s) { should eq "12 Angry Men - классический фильм, режиссер Sidney Lumet\nOther films of producer:\n  Network\n  Dog Day Afternoon\n" }
 end
