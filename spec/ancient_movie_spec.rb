32262eee3cd1c8656cd0c48d1faac0f544a0ac84
Andrey Aleksandrov <postgred@gmail.com>
2016-10-15 15:31:10 +0300

    Rewrite specs

--- spec/ancient_movie_spec.rb
+++ spec/ancient_movie_spec.rb
@@ -1,19 +1,9 @@
 require 'spec_helper.rb'
-require_relative '../ancient_movie.rb'
+require_relative '../movies.rb'
 
 RSpec.describe AncientMovie do
-  subject(:movie) { AncientMovie.new({url: 'http://imdb.com/title/tt0012349/?ref_=chttp_tt_96',
-                                      title: 'The Kid',
-                                      year: 1921,
-                                      country: 'USA',
-                                      date: '1921-02-06',
-                                      genre: 'Comedy,Drama,Family',
-                                      duration: '68 min',
-                                      stars: '8.4',
-                                      producer: 'Charles Chaplin',
-                                      actors: 'Charles Chaplin,Edna Purviance,Jackie Coogan'
-                                     },[]
-                                    )}
+  before { @films = MovieCollection.new('./spec/movies.txt') }
+  subject(:movie) { @films.filter(year: 1900..1945)[0] }
 
   its(:to_s) { should eq "#{movie.title} - старый фильм (#{movie.year} год)" }
 end
---
+++ spec/moden_movie_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper.rb'
+require_relative '../movies.rb'
+
+RSpec.describe ModernMovie do
+  before { @films = MovieCollection.new('./spec/movies.txt') }
+  subject(:movie) { @films.filter(year: 1969..2000)[0] }
+
+  its(:to_s) { should eq "The Shawshank Redemption - современное кино, играют: Tim Tobbins, Morgan Freeman, Bob Gunton" }
+end
+
