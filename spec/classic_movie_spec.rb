668c4a40cabfe6e60366c5177858987d9d1f8bce
Andrey Aleksandrov <postgred@gmail.com>
2016-10-15 14:46:33 +0300

    Spec for classic movie

--- spec/classic_movie_spec.rb
+++ spec/classic_movie_spec.rb
@@ -1,19 +1,9 @@
 require 'spec_helper.rb'
-require_relative '../classic_movie.rb'
+require_relative '../movies.rb'
 
 RSpec.describe ClassicMovie do
-  subject(:movie) { ClassicMovie.new({url: 'http://imdb.com/title/tt0046911/?ref_=chttp_tt_190',
-                                        title: 'Diabolique',
-                                        year: 1955,
-                                        country: 'France',
-                                        date: '1955-11-21',
-                                        genre: 'Drama,Horror,Thriller',
-                                        duration: '116 min',
-                                        start: '8.2',
-                                        producer: 'Henri-Georges Clouzot',
-                                        actors: 'Simone Signoret,Véra Clouzot,Paul Meurisse'
-                                       },[])
-                     }
+  before { @films = MovieCollection.new('./spec/movies.rb') }
+  subject(:movie) { @films.filter(:year, 1946..1968)[0] }
 
-  its(:to_s) { should eq "#{movie.title} - классический фильм, режиссер #{movie.producer}" }
+  its(:to_s) { should eq "Angry Men - классический фильм, режиссер Sidney Lumet\nOther films of producer:\n  Network\n  Dog Day Afternoon" }
 end
