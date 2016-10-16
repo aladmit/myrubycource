72ab70881b6ba1b79a6888f7bd60750d374b674f
Andrey Aleksandrov <postgred@gmail.com>
2016-10-16 17:53:41 +0300

    Test for netflix#show

--- spec/netflix_spec.rb
+++ spec/netflix_spec.rb
@@ -3,6 +3,17 @@ require_relative '../netflix.rb'
 
 RSpec.describe Netflix do
   subject(:netflix) { Netflix.new('./spec/movies.txt')}
+  let(:test_movie) { ModernMovie.new({url: "http://imdb.com/title/tt0091763/?ref_=chttp_tt_170",
+                                      title: "Platoon",
+                                      year: 1986,
+                                      country: "UK",
+                                      date: "1987-02-06",
+                                      genre: "Drama,War",
+                                      duration: 120,
+                                      producer: "Oliver Stone",
+                                      actors: "Charlie Sheen,Tom Berenger,Willem Dafoe"
+                                     },netflix.all) }
+
 
   context '#show' do
     it 'how some film now' do
@@ -11,6 +22,11 @@ RSpec.describe Netflix do
 
     it 'return random film' do
       expect((1..10).collect { netflix.show({}) }.uniq.count).not_to eq 1
+    end
+
+    it 'use filter' do
+      expect(netflix).to receive(:filter).with(producer: 'Oliver Stone', period: :modern).and_return([test_movie])
+      expect(netflix.show({producer: 'Oliver Stone', period: :modern})).to eq "Now showing: #{test_movie.to_s}"
     end
   end
 
