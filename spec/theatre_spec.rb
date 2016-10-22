deaca0133a741e2be37f56172b88cb2fd011a5c5
Andrey Aleksandrov <postgred@gmail.com>
2016-10-22 23:50:00 +0300

    Test for Theatre#filter_by_time

--- spec/theatre_spec.rb
+++ spec/theatre_spec.rb
@@ -15,17 +15,45 @@ RSpec.describe Theatre do
 
     it 'return ancient film in the morning' do
       hours = 8..12
-      expect(hours.map { |hour| theatre.show("#{hour}:00") }.map(&:class)).to all eq AncientMovie
+      filteres_films = hours
+                        .map { |hour| theatre.filter_by_time("#{hour}:30") }
+                        .flatten.uniq.map(&:title).join(' ')
+      expect(filteres_films).to include(theatre.show("8:30").match(/(?<=Now showing: )[A-Za-z ]*/).to_s)
     end
 
     it 'return Comedy and Action in the middle of day' do
       hours = 13..15
-      expect(hours.map { |hour| theatre.show("#{hour}:00") }.map(&:genre).flatten.uniq).to include("Action", "Comedy")
+      filteres_films = hours
+                        .map { |hour| theatre.filter_by_time("#{hour}:30") }
+                        .map { |film| "Now showing: #{film.to_s}" }
+      expect(filteres_films).to include(theatre.show("14:30").match(/(?<=Now showing: )[A-Za-z ]*/).to_s)
     end
 
     it 'return Drama and Horror in the evening' do
       hours = 16..20
-      expect(hours.map { |hour| theatre.show("#{hour}:00") }.map(&:genre).flatten.uniq).to include("Horror", "Drama")
+      filteres_films = hours
+                        .map { |hour| theatre.filter_by_time("#{hour}:30") }
+                        .map { |film| "Now showing: #{film.to_s}" }
+      expect(filteres_films).to include(theatre.show("18:30").match(/(?<=Now showing: )[A-Za-z ]*/).to_s)
+    end
+  end
+
+  context '#filter_by_time' do
+    subject(:theatre) { Theatre.new('./spec/movies.txt')}
+
+    it 'return ancient film in the morning' do
+      hours = 8..12
+      expect(hours.map { |hour| theatre.filter_by_time("#{hour}:00") }.flatten.map(&:class)).to all eq AncientMovie
+    end
+
+    it 'return Comedy and Action in the middle of day' do
+      hours = 13..15
+      expect(hours.map { |hour| theatre.filter_by_time("#{hour}:00") }.flatten.map(&:genre).flatten.uniq).to include("Action", "Comedy")
+    end
+
+    it 'return Drama and Horror in the evening' do
+      hours = 16..20
+      expect(hours.map { |hour| theatre.show("#{hour}:00") }.flatten.map(&:genre).flatten.uniq).to include("Horror", "Drama")
     end
   end
 end
