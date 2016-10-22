0fe90d1bb657d68009d5bdb4e523ac022accab69
Andrey Aleksandrov <postgred@gmail.com>
2016-10-22 21:43:11 +0300

    Test #show theatre with time

--- spec/theatre_spec.rb
+++ spec/theatre_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper.rb'
 require_relative '../theatre.rb'
 
-RSpec.describe Netflix do
+RSpec.describe Theatre do
   context '#show' do
     subject(:theatre) { Theatre.new('./spec/movies.txt')}
 
@@ -11,6 +11,21 @@ RSpec.describe Netflix do
 
     it 'return random film' do
       expect((1..10).collect { theatre.show }.uniq.count).not_to eq 1
+    end
+
+    it 'return ancient film in the morning' do
+      hours = 8..12
+      expect(hours.map { |hour| theatre.show("#{hour}:00") }.map(&:class)).to all eq AncientMovie
+    end
+
+    it 'return Comedy and Action in the middle of day' do
+      hours = 13..15
+      expect(hours.map { |hour| theatre.show("#{hour}:00") }.map(&:genre).flatten.uniq).to include("Action", "Comedy")
+    end
+
+    it 'return Drama and Horror in the evening' do
+      hours = 16..20
+      expect(hours.map { |hour| theatre.show("#{hour}:00") }.map(&:genre).flatten.uniq).to include("Horror", "Drama")
     end
   end
 end
