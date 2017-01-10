afcc0b10c4edec81acb6248c036b9add31814861
Andrey Aleksandrov <postgred@gmail.com>
2017-01-10 21:21:16 +0300

    Install vcr and webmock

---
+++ spec/imdb_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper.rb'
+
+RSpec.describe IMDBClient do
+  let(:client) { IMDBClient.new }
+
+  describe '#movies_list' do
+    it 'should return all list of top rated movies' do
+      VCR.use_cassette('imdb/list') do
+        list = client.movies_list
+
+        expect(list.length).to eq 250
+      end
+    end
+
+    it 'shoud contain title and link' do
+      VCR.use_cassette('imdb/list') do
+        list = client.movies_list
+
+        expect(list.first[:title]).not_to be_empty
+        expect(list.first[:link]).no_to be_empty
+      end
+    end
+  end
+end
