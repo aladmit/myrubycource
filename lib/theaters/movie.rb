3eefa19e88e006e7ab73e296a77fb2156aeadbea
Andrey Aleksandrov <postgred@gmail.com>
2017-01-29 21:36:10 +0300

    Add Yard doc for movie

--- lib/theaters/movie.rb
+++ lib/theaters/movie.rb
@@ -19,18 +19,33 @@ module Theaters
       attribute :collection
     end
 
+    # The month of a movie
+    #
+    # @return [Integer] the month of creation
     def month
       date.month
     end
 
+    # Price of a movie
+    #
+    # @return [Integer] a price
     def price
       self.class::PRICE
     end
 
+    # Period of a movie
+    # Example: :modern, :ancient, :new, :classic
+    #
+    # @return [Symbol] the period
     def period
       self.class.to_s.match(/(?<=Theaters::Movie::).*/).to_s.downcase.to_sym
     end
 
+    # Check contains filter in a movie
+    #
+    # @param filter [Symbol] Symbol is movies field
+    # @param value [String] String is a value
+    # @return [Boolean]
     def matches?(filter, value)
       if send(filter).is_a? Array
         send(filter).any? { |v| value === v }
@@ -39,6 +54,10 @@ module Theaters
       end
     end
 
+    # Check period and create a movie
+    #
+    # @param [Hash] fields of the movie
+    # @return [Object] the movie
     def self.create(fields)
       case fields[:year].to_i
       when 1900..1945
@@ -56,6 +75,10 @@ module Theaters
       "#{title}: #{producer} (#{date}; #{genre.join('/')}) - #{duration} min"
     end
 
+    # Check existence of a genre
+    #
+    # @param [String] name of the genre
+    # @return [Boolean]
     def genre?(name)
       raise GenreDoesNotExist unless @collection.genres.include?(name)
       genre.include?(name)
